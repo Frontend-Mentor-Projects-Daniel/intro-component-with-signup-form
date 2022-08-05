9a2f35bd51af3d5606f1db077309f171e1f2f637
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-08-05 17:50:53 -0400

    make edits in order to account for custom error messages

---
+++ develop/js/script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('[data-form]');
+const inputFirstName = document.querySelector('[data-form-first-name]');
+const inputLastName = document.querySelector('[data-form-last-name]');
+const inputEmail = document.querySelector('[data-form-email]');
+const inputPassword = document.querySelector('[data-form-password]');
+const buttonSubmit = document.querySelector('[data-form-submit]');
